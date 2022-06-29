4531ab266fd22f90056d15dac8b0d58be9126170
chan <chandramohan131198@gmail.com>
2022-06-29 08:49:57 +0530

    Day 1 Completed

---
+++ 01-Day/datatypes.js
@@ -0,0 +1,16 @@
+let Name = 'Chandra Mohan';
+let isLearner = true;
+let yourAge = 23;
+let hasGoodHabit;
+let nullVar=null;
+
+console.log(typeof(Name),typeof(age),typeof(isLearner),typeof(hasGoodHabit),typeof(nullVar));
+
+let firstName = 'chandra mohan';
+let lastName = 'S';
+let maritalStatus = 'single';
+let country = 'India';
+let myAge = 23;
+
+console.log('I am '+myAge+' years old');
+console.log('Your are '+yourAge+' years old');
---
+++ 01-Day/variable.js
@@ -0,0 +1,6 @@
+let myName = 'Chandra Mohan';
+let isProgrammer = true;
+let hasSuperpower;
+let nullVaraibale=null;
+
+console.log(myName,isProgrammer,hasSuperpower,nullVaraibale);
